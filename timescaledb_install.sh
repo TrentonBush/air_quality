4683b1d92d69618dde4cff4623f7b3f05239f84a
t-desktop <tpbush@gmail.com>
2021-01-25 14:41:01 -0800

    add config setup to install script

--- timescaledb_install.sh
+++ timescaledb_install.sh
@@ -12,3 +12,10 @@ git checkout 2.0.0
 ./bootstrap -DREGRESS_CHECKS=OFF -DWARNINGS_AS_ERRORS=OFF
 cd ./build && make  
 sudo make install
+
+sudo -i -u postgres
+psql -d postgres -c "SHOW config_file;"
+echo "NOT DONE! In the above config file, uncomment the line: shared_preload_libraries = 'timescaledb'"
+# it was: /etc/postgresql/12/main/postgresql.conf
+echo "example: nano /etc/postgresql/12/main/postgresql.conf"
+echo "then restart postgres: sudo systemctl restart postgresql.service"
