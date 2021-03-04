c3c7596d8edaf5631561db80cb8cbf61569611b6
t-desktop <tpbush@gmail.com>
2021-03-04 02:27:41 -0800

    refactor to make base API classes

--- air-quality/bmp280.py
+++ air-quality/bmp280.py
@@ -1,16 +1,25 @@
 """Driver for the Bosch BMP280 pressure sensor"""
 from smbus2 import SMBus
-from abc import ABC, abstractmethod
-import sys
 
-from .i2c_base import Device, Register, Field, Encoder, LookupTable, _bit_mask
+from .i2c_base import (
+    Device,
+    Register,
+    Field,
+    Encoder,
+    LookupTable,
+    _bit_mask,
+    BaseDeviceAPI,
+    BaseRegisterAPI,
+)
 
 
 class Int16(Encoder):
     """Signed 16 bit integer"""
+
     @staticmethod
     def encode(value: int, field) -> bytes:
-        pass # relevant field is read-only
+        pass  # relevant field is read-only
+
     @staticmethod
     def decode(value: bytes, field) -> int:
         if len(value) != 2:
@@ -21,9 +30,11 @@ class Int16(Encoder):
 
 class UInt16(Encoder):
     """Unsigned 16 bit integer"""
+
     @staticmethod
     def encode(value: int, field) -> bytes:
-        pass # relevant field is read-only
+        pass  # relevant field is read-only
+
     @staticmethod
     def decode(value: bytes, field) -> int:
         if len(value) != 2:
@@ -32,86 +43,48 @@ class UInt16(Encoder):
         return int.from_bytes(value, BMP280.hardware.byte_order)
 
 
-class RegisterAPI(ABC):
-    def __init__(self, bmp280: BMP280, reg_name: str):
-        self._reg = bmp280.hardware.registers[reg_name]
-        self._cached = {field.name: None for field in self._reg.fields.values()}
-    
-    @abstractmethod
-    def write(self):
-        pass
-    @abstractmethod
-    def read(self):
-        pass
-
-class ConfigAPI(RegisterAPI):
-    def write(self, temp_standby_ms = 4000, iir_filter_const = 8, spi = False):
-        """write to the config register
-
-        Args:
-            temp_standby_ms (int, optional): time (unit: ms) between temperature measurements in constant sampling mode. Possible values are [0.5, 62.5, 125, 250, 500, 1000, 2000, 4000]. Defaults to 4000.
-            iir_filter_const (int, optional): IIR filter coefficient. Higher -> smoother. Possible values are [0, 2, 4, 8, 16]. Defaults to 8.
-            spi (bool, optional): Enable SPI interface, which means none of this code will work. Defaults to False.
-        """
-        raise NotImplementedError
-
-    def read(self):
-        """read current config"""
-        raise NotImplementedError
-
-
-class BMP280(object):
+class BMP280(BaseDeviceAPI):
     hardware = Device(
         "bmp280",
         chip_id=0x58,
         i2c_addresses={0: 0x76, 1: 0x77},  # address pin logic level 0, 1 (GND, VCC)
-        registers={
-            "chip_id": Register(
-                "chip_id", 0xD0, fields=(Field("id")), read_only=True, volatile=False
-            ),
-            "reset": Register("reset", 0xE0, fields=(Field("reset"))),
-            "status": Register(
+        registers=(
+            Register("chip_id", 0xD0, fields=[Field("id")], read_only=True, volatile=False),
+            Register("reset", 0xE0, fields=[Field("reset")]),
+            Register(
                 "status",
                 0xF3,
-                fields=(
+                fields=[
                     Field("measuring", bit_mask=0b00001000),  # 1 when conversion is running
-                    Field(
-                        "im_update", bit_mask=0b00000001
-                    ),  # 1 when NVM data is being copied
-                ),
+                    Field("im_update", bit_mask=0b00000001),  # 1 when NVM data is being copied
+                ],
                 read_only=True,
             ),
-            "ctrl_meas": Register(
+            Register(
                 "ctrl_meas",
                 0xF4,
-                fields=(
+                fields=[
                     Field(
                         "osrs_t",
                         bit_mask=0b11100000,  # Temperature oversampling
-                        encoder=LookupTable(
-                            {1: 0b001, 2: 0b010, 4: 0b011, 8: 0b100, 16: 0b101}
-                        ),
+                        encoder=LookupTable({1: 0b001, 2: 0b010, 4: 0b011, 8: 0b100, 16: 0b101}),
                     ),
                     Field(
                         "osrs_p",
                         bit_mask=0b00011100,  # Pressure oversampling
-                        encoder=LookupTable(
-                            {1: 0b001, 2: 0b010, 4: 0b011, 8: 0b100, 16: 0b101}
-                        ),
+                        encoder=LookupTable({1: 0b001, 2: 0b010, 4: 0b011, 8: 0b100, 16: 0b101}),
                     ),
                     Field(
                         "mode",
                         bit_mask=0b00000011,  # Power mode
-                        encoder=LookupTable(
-                            {"sleep": 0b00, "forced": 0b10, "normal": 0b11}
-                        ),
+                        encoder=LookupTable({"sleep": 0b00, "forced": 0b10, "normal": 0b11}),
                     ),
-                ),
+                ],
             ),
-            "config": Register(
+            Register(
                 "config",
                 0xF5,
-                fields=(
+                fields=[
                     Field(
                         "t_sb",
                         bit_mask=0b11100000,  # Temp standby duration in 'normal' power mode
@@ -130,22 +103,22 @@ class BMP280(object):
                     ),
                     Field("filter", bit_mask=0b00011100),  # time constant of the IIR filter
                     Field("spi3w_en", bit_mask=0b0000001),  # Enable 3-wire SPI interface
-                ),
+                ],
             ),
-            "data": Register(
+            Register(
                 "data",
                 0xF7,
-                fields=(
+                fields=[
                     Field("temperature", byte_index=(3, 5), bit_mask=0xFFFFF0),
                     Field("pressure", byte_index=(0, 2), bit_mask=0xFFFFF0),
-                ),
-                bit_width=48,
+                ],
+                n_bits=48,
                 read_only=True,
             ),
-            "calibration": Register(
+            Register(
                 "calibration",
                 0x88,
-                fields=(
+                fields=[
                     Field("dig_t1", byte_index=(0, 1), encoder=UInt16),  # 0x88 0x89
                     Field("dig_t2", byte_index=(2, 3), encoder=Int16),  # 0x8A 0x8B
                     Field("dig_t3", byte_index=(4, 5), encoder=Int16),  # 0x8C 0x8D
@@ -158,19 +131,30 @@ class BMP280(object):
                     Field("dig_p7", byte_index=(18, 19), encoder=Int16),  # 0x9A 0x9B
                     Field("dig_p8", byte_index=(20, 21), encoder=Int16),  # 0x9C 0x9D
                     Field("dig_p9", byte_index=(22, 23), encoder=Int16),  # 0x9E 0x9F
-                ),
-                bit_width=192,
+                ],
+                n_bits=192,
                 read_only=True,
                 volatile=False,
             ),
-        },
+        ),
     )
 
     def __init(self, i2c_interface: SMBus, address_pin_level: int = 0):
-        self._i2c_interface = i2c_interface
-        self._address_level = address_pin_level
-        self.address = BMP280.hardware.i2c_addresses[self._address_level]
-        self._cached = {
-            register.name: {field.name: None for field in register.fields.values()}
-            for register in BMP280.hardware.registers.values()
-        }
+        super().__init__(i2c_interface, address_pin_level)
+        self.config = ConfigAPI(self, "config")
+
+
+class ConfigAPI(BaseRegisterAPI):
+    def write(self, temp_standby_ms=4000, iir_filter_const=8, spi=False):
+        """write to the config register
+
+        Args:
+            temp_standby_ms (int, optional): time (unit: ms) between temperature measurements in constant sampling mode. Possible values are [0.5, 62.5, 125, 250, 500, 1000, 2000, 4000]. Defaults to 4000.
+            iir_filter_const (int, optional): IIR filter coefficient. Higher -> smoother. Possible values are [0, 2, 4, 8, 16]. Defaults to 8.
+            spi (bool, optional): Enable SPI interface, which means none of this code will work. Defaults to False.
+        """
+        raise NotImplementedError
+
+    def read(self):
+        """read current config"""
+        raise NotImplementedError
--- air-quality/i2c_base.py
+++ air-quality/i2c_base.py
@@ -1,7 +1,9 @@
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
-from typing import Dict, Tuple, Type, Optional
+from dataclasses import dataclass, InitVar
+from typing import Dict, Tuple, Optional, Any, Sequence
 from collections.abc import Mapping
+from smbus2 import SMBus
+
 
 def _bit_mask(value: bytes, bit_mask: Optional[int]) -> bytes:
     if bit_mask is None:
@@ -12,28 +14,24 @@ def _bit_mask(value: bytes, bit_mask: Optional[int]) -> bytes:
 class Encoder(ABC):
     """base class for encode/decode methods to convert between human- and machine-readable data"""
 
-    @staticmethod
     @abstractmethod
-    def encode(value, field: Field) -> bytes:
+    def encode(self, value, field) -> bytes:
         """encode human-readable value to machine value"""
-        pass
+        ...
 
-    @staticmethod
     @abstractmethod
-    def decode(value: bytes, field: Field):
+    def decode(self, value: bytes, field) -> Any:
         """decode machine value to human-readable value"""
-        pass
+        ...
 
 
 class PassThroughEncoder(Encoder):
     """returns values unchanged"""
 
-    @staticmethod
-    def encode(value):
+    def encode(self, value, field):
         return value
 
-    @staticmethod
-    def decode(value):
+    def decode(self, value, field):
         return value
 
 
@@ -43,29 +41,34 @@ class LookupTable(Encoder):
     def __init__(self, lookup_table: dict):
         self.lookup_table = lookup_table
 
-    def encode(self, value):
+    def encode(self, value, field):
         return self.lookup_table[value]
 
-    def decode(self, value):
+    def decode(self, value, field):
         for k, v in self.lookup_table.items():
             if v == value:
                 return k
         raise ValueError("{} not in lookup table".format(value))
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, eq=False)
 class Field(object):
-    """Store immutable config for a field or flag in an i2c register"""
+    """Immutable properties of a field or flag in an i2c register"""
+
     name: str
     byte_index: Tuple[int, ...] = (0,)
     bit_mask: Optional[int] = None
-    encoder: Type[Encoder] = PassThroughEncoder  # uses static methods
-    bit_width = 8
-    read_only = False
+    encoder: Encoder = PassThroughEncoder()
+    n_bits: int = 8
+    read_only: bool = False
 
-    def __post_init__(self):
-        self._slice = slice(byte_index[0], byte_index[-1] + 1) # for slicing bytes from register
-    
+    def __post_init__(self) -> None:
+        # NOTE: must use __setattr__ due to quirk of frozen dataclasses
+        # for slicing bytes from register
+        object.__setattr__(
+            self, "_slice", slice(self.byte_index[0], self.byte_index[-1] + 1)
+        )
+
     def encode(self, value) -> bytes:
         return self.encoder.encode(value, self)
 
@@ -73,23 +76,140 @@ class Field(object):
         return self.encoder.decode(value, self)
 
 
-@dataclass(frozen=True)
+@dataclass(eq=False)
 class Register(object):
-    """Store immutable config for an i2c register"""
+    """Immutable properties of an i2c register"""
+
     name: str
     address: int
-    fields: Dict[str, Field]
-    bit_width = 8
-    read_only = False
-    volatile = True
+    fields: InitVar[Sequence[Field]] # InitVar not in __repr__
+    n_bits: int = 8
+    read_only: bool = False
+    volatile: bool = True
+
+    def __post_init__(self, fields) -> None:
+        self.fields: Dict[str, Field] = {field.name: field for field in fields}
 
 
-@dataclass(frozen=True)
+@dataclass(eq=False)
 class Device(object):
-    """Store immutable config for an I2C device"""
+    """Immutable properties of an I2C device"""
+
     name: str
     chip_id: int
     i2c_addresses: Mapping[int, int]
-    registers: Dict[str, Register]
-    byte_order = 'big'
-    bit_width: int = 8
+    registers: Sequence[Register]  # not using InitVar makes mypy angry
+    byte_order: str = "big"
+    word_size: int = 8
+
+    def __post_init__(self) -> None:
+        new_vals = {register.name: register for register in self.registers}
+        self.registers: Dict[str, Register] = new_vals
+
+
+class BaseDeviceAPI(ABC):
+    """base class for making APIs for an i2c hardware device"""
+
+    @property
+    @classmethod
+    @abstractmethod
+    def hardware(cls) -> Device:  # abstract class attribute
+        ...
+
+    def __init(self, i2c_interface: SMBus, address_pin_level: int = 0):
+        self._i2c = i2c_interface
+        self._address_level = address_pin_level
+        # TODO: check if I need type(self) here:
+        self.address = self.hardware.i2c_addresses[self._address_level]
+
+    def _i2c_write(self, register: Register, values: bytes):
+        self._i2c.write_i2c_block_data(self.address, register.address, values)
+
+    def _i2c_read(self, register: Register):
+        return bytes(
+            self._i2c.read_i2c_block_data(
+                self.address,
+                register.address,
+                # TODO: check if I need type(self) here:
+                register.n_bits // self.hardware.word_size,
+            )
+        )
+
+    def _raw_bytes_to_field_values(
+        self, register: Register, reg_values: bytes
+    ) -> Dict[str, Any]:
+        raise NotImplementedError
+
+    def _field_values_to_raw_bytes(
+        self, register: Register, field_values: Dict[str, Any]
+    ) -> bytes:
+        raise NotImplementedError
+
+
+class BaseRegisterAPI(ABC):
+    """base class to provide documented .read() and .write() methods for registers of BaseDeviceAPI subclasses.
+
+    This class serves two purposes: 1) create a helpful API and 2) cache values.
+    A BaseDeviceAPI that contains BaseRegisterAPI attributes produces an API format like:
+    sensor.register.write(field_1=x, field_2=y)
+
+    This is so .write() can have a descriptive function signature and the docstring
+    has key info from the datasheet, like arg descriptions and appropriate values.
+
+    Example API:
+    bmp280.config.write(measurement_period_ms=250, iir_filter_const=8)
+    """
+
+    def __init__(self, parent_device: BaseDeviceAPI, reg_name: str):
+        self._parent_device = parent_device
+        self._reg = self._parent_device.hardware.registers[reg_name]
+        self._cached = {field.name: None for field in self._reg.fields.values()}
+
+    @abstractmethod
+    def write(self):
+        ...
+
+    @abstractmethod
+    def read(self):
+        ...
+
+
+"""
+Below is an implementation of Device with frozen=True.
+It shows/explains a conflict between frozen dataclasses and mypy
+and why I opted to remove the frozen parameter entirely.
+
+from dataclasses import dataclass, InitVar
+import dataclasses.field # weird import due to name collision
+
+@dataclass(frozen=True)
+class Device(object):
+
+    name: str
+    chip_id: int
+    i2c_addresses: Mapping[int, int]
+    _registers: InitVar[Sequence[Register]]
+    byte_order = "big"
+    word_size: int = 8
+    registers: Dict[str, Register] = dataclasses.field(default_factory=dict)
+    # Why I have both .registers and ._registers:
+    # short version: there is a conflict between frozen dataclasses and mypy
+
+    # long version:
+    # I wanted to simply do:
+    # registers: Sequence[Register]
+    # then in __post_init__:
+    # self.registers = {register.name: register for register in self.registers}
+
+    # But frozen dataclasses remove attribute assignment, so that doesn't work.
+    # Instead, I had use object.__setattr__ (per docs: https://docs.python.org/3/library/dataclasses.html#frozen-instances)
+    # object.__setattr__(self, "registers", {register.name: register for register in self.registers})
+    # But mypy can't follow dynamic assignment, and thinks it's still Sequence[Register]
+
+    # The solution here makes a temporary (InitVar) _registers attr and an empty dict
+    # then fills the dict in __post_init__. But this makes a confusing API :(
+    # So I dropped the frozen=True instead.
+    def __post_init__(self) -> None:
+        for register in self._registers:
+            self.registers[register.name] = register
+"""
