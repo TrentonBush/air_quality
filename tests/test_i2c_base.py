c9c15fe7b3172c7d71cff4a6f7a6220d241e9bb5
t-desktop <tpbush@gmail.com>
2021-03-05 15:15:30 -0800

    UIntEncoder and Field encode/decode methods

--- tests/test_i2c_base.py
+++ tests/test_i2c_base.py
@@ -29,23 +29,51 @@ def test__count_trailing_zeros__edge_all_zeros():
         _count_trailing_zeros(mask)
 
 
-@pytest.fixture
-def basic_Field():
-    f = Field(
-        "my_field", byte_index=(0, 1), bit_mask=0b00111000, encoder=UIntEncoder(), read_only=False
-    )
-    yield f
+def test_UIntEncoder__encode_normal_one_byte():
+    field = Field("my_field", byte_index=(0,))
+    encoder = UIntEncoder()
+    input_value = 0b10000001
+    expected = b"\x81"
+    actual = encoder.encode(input_value, field)
+    assert actual == expected
 
 
-def test_Field___eq__(basic_Field):
-    f1 = basic_Field
+def test_UIntEncoder__encode_normal_multi_byte():
+    field = Field("my_field", byte_index=(0, 1, 2))
+    encoder = UIntEncoder()
+    input_value = 0xABCDEF
+    expected = b"\xAB\xCD\xEF"
+    actual = encoder.encode(input_value, field)
+    assert actual == expected
+
+
+def test_UIntEncoder__decode_normal_one_byte():
+    field = Field("my_field", byte_index=(0,))
+    encoder = UIntEncoder()
+    input_value = b"\x81"
+    expected = 0b10000001
+    actual = encoder.decode(input_value, field)
+    assert actual == expected
+
+
+def test_UIntEncoder__decode_normal_multi_byte():
+    field = Field("my_field", byte_index=(0, 1, 2))
+    encoder = UIntEncoder()
+    input_value = b"\xAB\xCD\xEF"
+    expected = 0xABCDEF
+    actual = encoder.decode(input_value, field)
+    assert actual == expected
+
+
+def test_Field___eq__():
+    f1 = Field("my_field", byte_index=(0, 1), bit_mask=0b00111000)
     f2 = Field("new_field")
     assert not f1 == f2
     assert f1 == f1
 
 
-def test_Field___repr__(basic_Field):
-    expected = basic_Field
+def test_Field___repr__():
+    expected = Field("my_field", byte_index=(0, 1), bit_mask=0b00111000)
     actual = eval(repr(expected))
     assert actual == expected
 
@@ -117,4 +145,36 @@ def test_Field__encode_mask__normal_multi_byte():
     input_bytes = (0x00FF00).to_bytes(3, "big")
     expected = (0x0FF000).to_bytes(3, "big")
     actual = f._encode_mask(input_bytes)
+    assert actual == expected
+
+
+def test_Field_with_UIntEncoder__encode_normal_one_byte():
+    field = Field("", bit_mask=0b11110000, encoder=UIntEncoder())
+    input_value = 0b00000011
+    expected = bytes([0b00110000])
+    actual = field.encode(input_value)
+    assert actual == expected
+
+
+def test_Field_with_UIntEncoder__encode_normal_multi_byte():
+    field = Field("", byte_index=(0, 1, 2), bit_mask=0x0FFFF0, encoder=UIntEncoder())
+    input_value = 0x00FF00
+    expected = (0x0FF000).to_bytes(3, "big")
+    actual = field.encode(input_value)
+    assert actual == expected
+
+
+def test_Field_with_UIntEncoder__decode_normal_one_byte():
+    field = Field("", bit_mask=0b11110000, encoder=UIntEncoder())
+    input_value = bytes([0b00111100])
+    expected = 0b00000011
+    actual = field.decode(input_value)
+    assert actual == expected
+
+
+def test_Field_with_UIntEncoder__decode_normal_multi_byte():
+    field = Field("", byte_index=(0, 1, 2), bit_mask=0x0FFFF0, encoder=UIntEncoder())
+    input_value = (0xFFF000).to_bytes(3, "big")
+    expected = 0x00FF00
+    actual = field.decode(input_value)
     assert actual == expected
