e839c64c445f6596dbe1aeb62483bc577c58e933
Trenton Bush <tpbush@gmail.com>
2021-03-18 10:18:37 +0000

    pin versions

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="sensors",
+    python_requires=">=3.7",
     packages=find_packages(),
     version="0.1.0",
     description="drivers for assorted air quality sensors",
